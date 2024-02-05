94565d284bfb89133d73525cd7eda759cf4a2647
Avani <avani.gopalnagar@gmail.com>
2024-02-05 17:42:11 +0530

    landing page implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import Footer from  "./pages/landing/components/footer"
 import Login from "./pages/loginpages/loginpage"
+import Offer from "./pages/landing/components/offer"
+import Loginpart from "./pages/landing/components/loginpart"
+import ImageSlider from "./pages/landing/components/imageslider";
+
 function App() {
   return (
-  
-     <Login/>
+   <div>
+    <Login/>
+
+    <div className="bg-black" >  
+ <Loginpart/>
+ <ImageSlider/>
+  <div className="flex justify-center px-60 p-16 bg-black"> <Offer/></div>
+  <Footer/>
+</div>
+</div>
   );
 }
 
